384b88599c8ce8b054eb0e86528088fa2aa107ff
TijsDeBelie <tijs.debelie@student.odisee.be>
2017-10-16 10:18:52 +0200

    Mysql connect is in orde, vraagt data op:
    events met jaar 2017 en maand oktober(10) kan gelijk welke dag zijn
    
    database structuur is id int(11), name varchar(255), date date

--- NOTUSED.php
+++ NOTUSED.php
@@ -59,7 +59,7 @@
     <?php
     /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-    $link = mysqli_connect("localhost", "root", "", "Tickets");
+    $link = mysqli_connect("localhost", "remote", "LXTNrdMSO75MvSl@xG2%58D!QaanY#", "Tickets");
 
     // Check connection
     if($link === false){
@@ -67,21 +67,21 @@ server with default setting (user 'root' with no password) */
     }
 
     // Attempt select query execution
-    $sql = "SELECT * from `events` where extract(year from `date`) = '2017' && extract(month from `date`) = '10'";
+    $sql = "SELECT * from `Events` where extract(year from `DATE`) = 2017 AND extract(month from `DATE`) = 10";
     if($result = mysqli_query($link, $sql)){
         if(mysqli_num_rows($result) > 0){
             echo "<table>";
             echo "<tr>";
             echo "<th>id</th>";
-            echo "<th>name</th>";
-            echo "<th>date</th>";
+            echo "<th>naam</th>";
+            echo "<th>datum</th>";
 
             echo "</tr>";
             while($row = mysqli_fetch_array($result)){
                 echo "<tr>";
-                echo "<td>" . $row['id'] . "</td>";
-                echo "<td>" . $row['name'] . "</td>";
-                echo "<td>" . $row['date'] . "</td>";
+                echo "<td>" . $row['ID'] . "</td>";
+                echo "<td>" . $row['NAAM'] . "</td>";
+                echo "<td>" . $row['DATE'] . "</td>";
 
                 echo "</tr>";
             }
