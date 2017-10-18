922126ffe0b656687f1b0acae615bd55c79af52d
TijsDeBelie <Tijsdebelie1997@gmail.com>
2017-10-18 18:57:12 +0200

    Add files via upload

---
+++ search.js
@@ -0,0 +1,28 @@
+function bevestig() {
+
+}
+
+
+
+console.log("Net voor de knop");
+$(document).ready(function(){
+    console.log("document is ready")
+    $("#bevestig").click(function(){
+        var x = $("#myMonth").val();
+        console.log(x);
+        console.log("Klik op de knop");
+        var userinput = $("#myMonth").val();
+        var year = userinput.substring(0, 4);
+        var month = userinput.substring(5);
+
+        $.post({
+            url:"select.php",
+            data: {year: year, month: month},
+            success:function(result,e){
+                console.log(result + e);
+                $("#adiv").html(result);
+
+            }
+        });
+    });
+});
---
+++ select.php
@@ -0,0 +1,46 @@
+<?php
+
+$year = $_POST["year"];
+$month = $_POST["month"];
+
+
+$link = mysqli_connect("localhost", "remote", "LXTNrdMSO75MvSl@xG2%58D!QaanY#", "Tickets");
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+// Attempt select query execution
+$sql = "SELECT * from `Events` where extract(year from `DATE`) = $year AND extract(month from `DATE`) = $month";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+        echo "<tr>";
+        echo "<th>id</th>";
+        echo "<th>naam</th>";
+        echo "<th>datum</th>";
+
+
+        echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+            echo "<td>" . $row['ID'] . "</td>";
+            echo "<td>" . $row['NAAM'] . "</td>";
+            echo "<td>" . $row['DATE'] . "</td>";
+
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+
+// Close connection
+mysqli_close($link);
+?>
