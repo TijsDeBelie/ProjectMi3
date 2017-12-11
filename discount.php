506f2ed8f8005f5a84751a6214bd3e9f3a02e34e
TijsDeBelie <tijs.debelie@student.odisee.be>
2017-12-11 19:42:47 +0100

    added discount.php

---
+++ discount.php
@@ -0,0 +1,20 @@
+<?php
+
+include('config.php');
+
+
+
+$value = $_POST["value"];
+$user = $_POST["user"];
+
+$sql ="UPDATE `User_info` SET `Thema` = '$value' WHERE username = '$user'";
+mysqli_query($db, $sql);
+$response['Thema'] = $value;
+
+
+$post_data = json_encode($response);
+
+echo $post_data;
+
+
+?>
